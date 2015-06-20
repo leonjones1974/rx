de867145f0de561f992ea4d1f4bf6aad22b51899
Leon Jones <leonj_24@yahoo.com>
2015-06-20 10:49:26 +0100

    Create capture project

---
+++ capture/src/main/java/com/cam/rx/capture/Dummy.java
@@ -0,0 +1,4 @@
+package com.cam.rx.capture;
+
+public class Dummy {
+}
---
+++ capture/src/test/java/com/cam/rx/capture/DummyTest.java
@@ -0,0 +1,13 @@
+package com.cam.rx.capture;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class DummyTest {
+
+    @Test
+    public void itShould_Fail() {
+        fail("Test");
+    }
+}
