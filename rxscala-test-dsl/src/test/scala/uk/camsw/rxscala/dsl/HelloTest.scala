c4a00e92e9974723cd1b5fba9a7bc42ecdf57c77
Leon Jones <leonj_24@yahoo.com>
2015-10-21 13:44:33 +0100

    Add scala test support Currently failing test

--- rxscala-test-dsl/src/test/scala/uk/camsw/rxscala/dsl/HelloTest.scala
+++ rxscala-test-dsl/src/test/scala/uk/camsw/rxscala/dsl/HelloTest.scala
@@ -1,7 +1,14 @@
 package uk.camsw.rxscala.dsl
 
-import org.scalatest.FunSpec
+import org.scalatest.{Matchers, FunSpec}
 
-class HelloTest extends FunSpec {
+class HelloTest
+  extends FunSpec
+  with Matchers {
 
+  describe("Anything") {
+    it("should fail") {
+      false shouldBe true
+    }
+  }
 }
